8fd5bb905f40acbbe3e97cfd84be5f0e8d8a7b1e
xhuyz <xhuyzdev@gmail.com>
2026-04-20 15:10:57 +0700

    update

--- config/opencode.nix
+++ config/opencode.nix
@@ -2,25 +2,15 @@
   plugins.opencode = {
     enable = true;
   };
-  keymaps = [
-    {
-      key = "<leader>e";
-      mode = [ "n" ];
-      action = "<cmd>lua Snacks.explorer()<CR>";
-      options = {
-        silent = true;
-        noremap = true;
-      };
-    }
-    {
-      key = "<leader>?";
-      mode = [ "n" ];
-      action = "<cmd>lua Snacks.picker.grep()<CR>";
-      options = {
-        silent = true;
-        noremap = true;
-      };
-    }
-  ];
 
+  extraConfigLua = ''
+    vim.g.opencode_opts = {
+      port = 12345,
+      server = {
+        start = function()
+          vim.fn.jobstart({ "opencode", "--port", "12345" })
+        end,
+      },
+    }
+  '';
 }
